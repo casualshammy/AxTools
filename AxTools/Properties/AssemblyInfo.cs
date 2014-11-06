a84dbb7ae03a2e5592a9fe8bfb24af2bdb0b35a3
casualshammy <none@none>
2014-11-06 21:10:25 +0000

    AxTools: fix for WowAccountsManager (changed account didn't save)

--- AxTools/Properties/AssemblyInfo.cs
+++ AxTools/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("11.17.11.0")]
+[assembly: AssemblyVersion("11.17.12.0")]
 //[assembly: AssemblyFileVersion("0.0.0.0")]
