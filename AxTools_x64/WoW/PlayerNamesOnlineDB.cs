ab95df9bd7d6ea94350703987e0654b3c14431d2
Alex Sommen <axio@axio.name>
2017-01-04 23:37:57 +0000

    AxTools_x64/WoW/PlayerNamesOnlineDB.cs: stub for remote redis DB

---
+++ AxTools_x64/WoW/PlayerNamesOnlineDB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AxTools.WoW.Internals;
+
+namespace AxTools.WoW
+{
+    internal static class PlayerNamesOnlineDB
+    {
+
+        internal static string GetPlayerName(WoWGUID playerGUID)
+        {
+            
+        }
+
+        internal static bool IsPlayerNameKnown(WoWGUID playerGUID)
+        {
+            
+        }
+
+        internal static void SendPlayerName(WoWGUID playerGUID, string name)
+        {
+            string base64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(name));
+
+        }
+
+    }
+}
